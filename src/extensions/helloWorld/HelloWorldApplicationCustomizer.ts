4b3082f67658a1a90bdaeb0cf9c706e530d7e6ef
Corey Roth <coreyroth@live.com>
2019-01-08 11:14:56 -0600

    Initial Commit.

---
+++ src/extensions/helloWorld/HelloWorldApplicationCustomizer.ts
@@ -0,0 +1,67 @@
+import { override } from '@microsoft/decorators';
+import { Log } from '@microsoft/sp-core-library';
+import {
+  BaseApplicationCustomizer, PlaceholderContent, PlaceholderName
+} from '@microsoft/sp-application-base';
+
+import * as strings from 'HelloWorldApplicationCustomizerStrings';
+
+const LOG_SOURCE: string = 'HelloWorldApplicationCustomizer';
+
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface IHelloWorldApplicationCustomizerProperties {
+  // This is an example; replace with your own property
+  testMessage: string;
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class HelloWorldApplicationCustomizer
+  extends BaseApplicationCustomizer<IHelloWorldApplicationCustomizerProperties> {
+  private _bottomPlaceholder: PlaceholderContent | undefined;
+
+  @override
+  public onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+
+    let message: string = this.properties.testMessage;
+    if (!message) {
+      message = '(No properties were provided.)';
+    }
+
+
+    this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
+
+    return Promise.resolve();
+  }
+
+  private _renderPlaceHolders(): void {
+    if (!this._bottomPlaceholder) {
+      this._bottomPlaceholder = this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Bottom,
+        { onDispose: this._onDispose }
+      );
+
+      // The extension should not assume that the expected placeholder is available.
+      if (!this._bottomPlaceholder) {
+        console.error("The expected placeholder (bottom) was not found.");
+        return;
+      }
+
+      if (this.properties) {
+        if (this._bottomPlaceholder.domElement) {
+          this._bottomPlaceholder.domElement.innerHTML = `
+          <div>Package test - please ignore.
+          </div>`;
+        }
+      }
+    }
+  }
+
+  private _onDispose(): void {
+    console.log('[HelloWorldApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.');
+  }
+}
